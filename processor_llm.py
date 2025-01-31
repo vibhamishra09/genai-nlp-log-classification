7ab6d3b6b6d1f1bb9e08041c3f297fc5e7f96425
Dhaval Patel <dhavalsays@gmail.com>
2025-01-31 16:52:57 -0500

    use deepseek instead of llama

--- processor_llm.py
+++ processor_llm.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 from groq import Groq
+import json
+import re
+
 
 load_dotenv()
 
@@ -13,17 +16,24 @@ def classify_with_llm(log_msg):
     """
     prompt = f'''Classify the log message into one of these categories: 
     (1) Workflow Error, (2) Deprecation Warning.
-    If you can't figure out a category, return "Unclassified". 
-    Only return the category name. No preamble. 
+    If you can't figure out a category, use "Unclassified".
+    Put the category inside <category> </category> tags. 
     Log message: {log_msg}'''
 
     chat_completion = groq.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
-        model="llama-3.3-70b-versatile",
+        # model="llama-3.3-70b-versatile",
+        model="deepseek-r1-distill-llama-70b",
         temperature=0.5
     )
 
-    return chat_completion.choices[0].message.content
+    content = chat_completion.choices[0].message.content
+    match = re.search(r'<category>(.*)<\/category>', content, flags=re.DOTALL)
+    category = "Unclassified"
+    if match:
+        category = match.group(1)
+
+    return category
 
 
 if __name__ == "__main__":
